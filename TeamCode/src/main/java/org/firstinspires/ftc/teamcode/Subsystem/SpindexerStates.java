3f91a4042c6ca0b290ae7da5f1e87f519aaf554e
Greek Question Mark <galaxyisfake@gmail.com>
2026-09-23 22:26:47 -0600

    Fix my incorrect statemachine code lolo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/SpindexerStates.java
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.Subsystem;
-
-public enum SpindexerStates {
-}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/RobotStates.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Util;
+
+public class RobotStates {
+    public enum SpindexerStates{
+        NEUTRAL,
+        P1,
+        P2,
+        P3,
+        P4,
+        ROTATING
+    }
+}
