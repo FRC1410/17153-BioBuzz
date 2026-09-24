a4f4f4acca6bcab9a3b701a0f5e81ef9bc64b3d2
Greek Question Mark <galaxyisfake@gmail.com>
2026-09-23 22:39:57 -0600

    Make some preliminary servo stuffs and IDs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/Spindexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/Spindexer.java
@@ -1,4 +1,16 @@
 package org.firstinspires.ftc.teamcode.Subsystem;
 
+import static org.firstinspires.ftc.teamcode.Util.ID.SPINDEXER_ID;
+
+import org.firstinspires.ftc.teamcode.Util.RobotStates.*;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 public class Spindexer {
+    private Servo dexter;
+
+    public void init(HardwareMap hardwareMap){
+        this.dexter = hardwareMap.get(Servo.class, SPINDEXER_ID);
+    }
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/ID.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.Util;
+
+public class ID {
+    public static final String SPINDEXER_ID = "1";
+}
